0f3e405cd84ecf79b37b18cd4cb087ed0f1450a0
sushil <parshid123@gmail.com>
2023-06-18 11:42:52 +0545

    function

--- work1/a1.js
+++ work1/a1.js
@@ -6,17 +6,17 @@ const PI=3.14;
 console.log(PI);
 
 // Question3
-let x=2;
-let y=2;
-let z=x+y;
-if(x==y)
-{
-    console.log(3*z);
-}
-else
-{
-    console.log(z);
-}
+// let x=2;
+// let y=2;
+// let z=x+y;
+// if(x==y)
+// {
+//     console.log(3*z);
+// }
+// else
+// {
+//     console.log(z);
+// }
 
 // Question4
 let m=21;
@@ -28,5 +28,19 @@ else
 {
     console.log("not  multiple");
 }
+
+// Question4
+const opr = (a,b) => {
+    console.log(a*b);
+    console.log(a/b);
+}
+opr(2,4);
+
+// question5
+let f= prompt("enter the value of f");
+// console.log(f);
+let c=(f-32)*5/9;
+console.log(c);
+
 
 
