7d1df48095a82ba742b10652910dca6669d06205
sushil <parshid123@gmail.com>
2023-06-18 11:03:17 +0545

    ifff

--- work1/a1.js
+++ work1/a1.js
@@ -4,3 +4,29 @@ console.log("hello world");
 // Question2
 const PI=3.14;
 console.log(PI);
+
+// Question3
+let x=2;
+let y=2;
+let z=x+y;
+if(x==y)
+{
+    console.log(3*z);
+}
+else
+{
+    console.log(z);
+}
+
+// Question4
+let m=21;
+if(m/3 || m/7)
+{
+    console.log("multiple of 3");
+}
+else
+{
+    console.log("not  multiple");
+}
+
+
